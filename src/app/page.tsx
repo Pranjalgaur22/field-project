d56b9befe5a1b685064ba12025b418659f1b2ee2
Pranjal Gaur <pranjalgaur2294@gmail.com>
2023-04-18 22:03:53 +0530

    gggg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <main className="bg-black">
-      <div className="min-h-screen w-full flex justify-center items-center">
-        <div className="flex flex-col gap-6 text-center">
-        <h2 className="font-semibold text-7xl z-10 text-white" >A Spiritual Odyssey</h2>
-        <h6 className="font-semibold text-4xl z-10 text-white" >Mathura-Vrindavan Adventure</h6>
+      <div className="min-h-screen w-full flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col gap-6 w-full justify-center text-center items-center">
+          <h2 className="font-semibold text-6xl lg:text-7xl z-10 text-white" >A Spiritual Odyssey</h2>
+          <h6 className="font-semibold text-2xl lg:text-4xl z-10 text-white" >Mathura-Vrindavan Adventure</h6>
         </div>
-        <button>Explore</button>
+        <button className="text-white border  border-white p-2 rounded-full">Explore</button>
       </div>
       <div className="h-screen w-full absolute top-0 overflow-hidden opacity-50">
             <video src="./video1.webm" autoPlay muted loop className="bg-cover" ></video>
